6e8050c396ef400d3a9bda5c8f37a79c0bc82f85
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-17 00:06:03 +0530

    add program 11

---
+++ 07_File_Handling/11_program.py
@@ -0,0 +1,5 @@
+# 11. Write a python program to rename a file to "renamed_by_python.txt".
+
+import os
+
+os.rename('07_File_Handling/files/11_file.txt', '07_File_Handling/files/renamed_by_python.txt')
