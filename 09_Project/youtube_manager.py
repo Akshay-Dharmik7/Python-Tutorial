a393720f4e71ad0173fba10ebcadcec85dda73e4
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-19 00:09:41 +0530

    create python project structure

---
+++ 09_Project/youtube_manager.py
@@ -0,0 +1,55 @@
+import json
+
+def load_data():
+    try:
+        with open('youtube.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos):
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos, file)
+
+def list_all_videos(videos):
+    for index, video in enumerate(videos, start=1):
+        print(f'{index}. ')
+
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+def main():
+    videos = load_data()
+
+    while True:
+        print('\n Youtube  Manager | choose an option ')
+        print('1. List all Youtube Videos ')
+        print('2. Add a Youtube Video ')
+        print('3. Update a Youtube Video Details')
+        print('4. Delete a video ')
+        print('5. Exit the App')
+
+        choice = input('Enter the choice')
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                add_video(videos)
+
+if __name__ == '__main__':
+    main()
