edfc180be03c3eeabd72e77a33021060ffe9eb8a
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-19 11:33:24 +0530

    Api handling in python

---
+++ 10_Handling_Apis/freeapi_access.py
@@ -0,0 +1,24 @@
+import requests
+
+def fetch_randon_user_freeapi():
+    url = 'https://api.freeapi.app/api/v1/public/randomusers/user/random'
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        user_name = user_data["login"]["username"]
+        user_country = user_data["location"]["country"]
+        return user_name, user_country
+    else:
+        raise Exception("Fail to fetch user data")
+
+def main():
+    try:
+        username, country = fetch_randon_user_freeapi()
+        print(f'Username: {username} \nCountry: {country}')
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+    main()
